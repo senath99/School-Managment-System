2538fa41e21d8bac5711d026baa4bc0d5f342220
Senath99 <senathweerasingha@gmail.com>
2021-04-07 01:41:29 +0530

    Service Subscriber committed

--- Bundles/ServiceSubscriber/src/servicesubscriber/ServiceActivator.java
+++ Bundles/ServiceSubscriber/src/servicesubscriber/ServiceActivator.java
@@ -1,1 +1,32 @@
-//adaaaaaaaaaaaaaaaa
+package servicesubscriber;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import servicepublisher.ServicePublisher;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class ServiceActivator extends AbstractUIPlugin {
+
+
+	ServiceReference servicReference;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Start Subscriber Service");
+		servicReference = context.getServiceReference(ServicePublisher.class.getName());
+		ServicePublisher servicePublisher = (ServicePublisher) context.getService(servicReference);
+		System.out.println(servicePublisher.publishService());
+	
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Good Bye!!");
+		context.ungetService(servicReference);
+	}
+
+	
+}
