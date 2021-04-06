e50a3318e0ee8ee5d853e16fa4e0ca8ee38e9e65
Senath99 <senathweerasingha@gmail.com>
2021-04-06 19:55:45 +0530

    Service Publisher Committed

---
+++ ServicePublisher/src/servicepublisher/ServicepublishImpl.java
@@ -0,0 +1,12 @@
+package servicepublisher;
+
+public class ServicepublishImpl implements ServicePublisher{
+
+	@Override
+	public String publishService() {
+		
+		return "Execute the publish service of ServicePublisher" ;
+	}
+
+	
+}
